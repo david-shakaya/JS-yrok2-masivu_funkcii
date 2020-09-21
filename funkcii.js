77cd5aa83b572e1485db55592edee198243cd0b3
david-shakaya <devidoz666@gmail.com>
2020-09-21 20:39:56 +0300

    +

---
+++ funkcii.js
@@ -0,0 +1,7 @@
+// Масивы
+// Узнать длину масива можно с помощью свойства .length
+const friends = ['kivi', 'david', 'alex', 'ajax'];
+console.log(friends.length); /* >.length - узнаем длину масива  */
+console.table(
+  friends,
+); /*> Показывает масив в виде таблицы. Индекс слева свойство справа  */
