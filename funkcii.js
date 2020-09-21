7260c31ac352f3cdb59d338eb0761c20cf04c55f
david-shakaya <devidoz666@gmail.com>
2020-09-22 00:42:38 +0300

    +

--- funkcii.js
+++ funkcii.js
@@ -1,7 +1,16 @@
 // Масивы
 // Узнать длину масива можно с помощью свойства .length
+/* const friends = ['kivi', 'david', 'alex', 'ajax'];
+console.log(friends.length); /* >.length - узнает длину масива  */
+// console.table(friends); */
+/*> Показывает масив в виде таблицы. Индекс слева свойство справа  */
+// *
+// *
+// Обращаемся к элементу масива
 const friends = ['kivi', 'david', 'alex', 'ajax'];
-console.log(friends.length); /* >.length - узнаем длину масива  */
-console.table(
-  friends,
-); /*> Показывает масив в виде таблицы. Индекс слева свойство справа  */
+console.log(friends[2]);
+/* В квадратных скобках индекс масива к которому обращяемся[alex]
+ *
+ */
+friends[1] = 'qweqwe'; /* перезаписываем елемент масива под индексом 1 */
+console.table(friends);
