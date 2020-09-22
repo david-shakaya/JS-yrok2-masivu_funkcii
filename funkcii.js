db2b3782900fd0454c2fcb0d08ac4e6f068910d5
david-shakaya <devidoz666@gmail.com>
2020-09-22 18:30:44 +0300

    +

--- funkcii.js
+++ funkcii.js
@@ -7,10 +7,105 @@ console.log(friends.length); /* >.length - узнает длину масива 
 // *
 // *
 // Обращаемся к элементу масива
-const friends = ['kivi', 'david', 'alex', 'ajax'];
-console.log(friends[2]);
+// const friends = ['kivi', 'david', 'alex', 'ajax'];
+// console.log(friends[2]);
 /* В квадратных скобках индекс масива к которому обращяемся[alex]
  *
  */
-friends[1] = 'qweqwe'; /* перезаписываем елемент масива под индексом 1 */
-console.table(friends);
+// friends[1] = 'qweqwe'; /* перезаписываем елемент масива под индексом 1 */
+// console.table(friends);
+/*
+ *
+ * - Перебор (итерация ) масива c помощю цыкла.
+ * -for - Если нужен индекс и нужно изменить елемент масива
+ * for...of - если не нужен индекс и ничего изменять не нужно
+ */
+// const friends = ['kivi', 'david', 'alex', 'ajax'];
+// const lengthIndex = friends.length; /* Переменная с длиной индекса */
+// for (let i = 0; i < lengthIndex; i += 1) {
+/*i < lengthIndex - Указали, что i менше длины индекса масива  */
+//   friends[i] += ' -1'; /* Пришиваем -1(изменяем) елементы масива */
+// }
+// console.log(friends);
+
+// const friends = ['kivi', 'david', 'alex', 'ajax'];
+// for (const friend of friends) {
+/* >>>>>> Конструкция помогает перебрать елементы масива */
+//   console.log(friend);
+// }
+/*
+ *
+ *
+ *----------------------ЗАДАЧА */
+// Посчитать общую сумму чисел
+// const cart = [200, 4, 4, 2];
+// let total = 0;
+// for (let i = 0; i < cart.length; i += 1) {
+//   console.log(cart[i]);
+//   total += cart[i];
+// }
+// console.log('Сумма:', total);
+
+// ТАЖЕ ЗАДАЧА только через -for...of
+// const carts = [200, 4, 4, 2];
+// let total = 0;
+// for (const cart of carts) {
+//   total += cart;
+// }
+// console.log(total);
+
+/* ----------------Задача АНАЛОГИЧНАЯ но(с процентами)
+Посчитать общую сумму чисел в масиве. От каждого числа в масиве отминусовать 10 процентов.
+Вывести на экран перечень чисел со скидкой и общую сумму без скидки.
+Вычесление процентов (Число / 100) * скидку% */
+//  const cart = [200, 4, 4, 2];
+// let total = 0;
+// for (let i = 0; i < cart.length; i += 1) {
+//   total = cart[i] + total;
+//   discount = (cart[i] / 100) * 10;
+//   console.log(cart[i] - discount);
+// }
+// console.log(total);
+/*
+ *
+ *
+ *
+ *
+ *------------------ЗАДАЧА
+ *Напиши скрипт который подсчитает сумму всех четных чисел в масиве
+ */
+/* const numbers = [2, 4, 10, 1, 47, 12, 7, 8];
+let total = 0;
+for (let i = 0; i < numbers.length; i += 1) {
+  console.log(numbers[i]);
+  if (numbers[i] % 2 === 0) {
+    total += numbers[i];
+  }
+}
+console.log('Четное!', total); 
+*
+*
+*
+*/
+const log = ['wewerw', 'sdfdf', 'ff3', 'sfsfww32'];
+const loginTofind = 'ff3';
+let message;
+
+for (let i = 0; i < log.length; i += 1) {
+  if (log[i] === loginTofind) {
+    message = 'Пользователь Найден';
+    break;
+  }
+  message = 'YTVF';
+}
+console.log(message);
+
+/* for (let i = 0; i < log.length; i += 1) {
+  if (log[i].includes(loginTofind) === true) {
+      console.log(`Пользовател с логином ${loginTofind} найден`);
+      
+  } else {
+    console.log(`Пользователя с таким ${loginTofind} логином  НЕТ`);
+  }
+}
+ */
