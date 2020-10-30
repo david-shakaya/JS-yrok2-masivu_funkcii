b01667e2432e2f1129d6f1ba083fa78315f0bb1a
david-shakaya <devidoz666@gmail.com>
2020-10-30 23:22:14 +0200

    +++

--- funkcii.js
+++ funkcii.js
@@ -2968,58 +2968,84 @@ arr - весь масив
 // const getUsersWithEyeColor = (array, color) => array.filter(({eyeColor})  => eyeColor === color)
 // console.log(getUsersWithEyeColor(users, 'blue'));
  
-const users =  [
-  {
-    id: '701b29c3-b35d-4cf1-a5f6-8b12b29a5081',
-    name: 'Moore Hensley',
-    email: 'moorehensley@indexia.com',
-    eyeColor: 'blue',
-    friends: ['Sharron Pace'],
-    isActive: false,
-    balance: 2811,
-    skills: ['ipsum', 'lorem'],
-    gender: 'male',
-    age: 37,
-  },
-  {
-    id: '7a3cbd18-57a1-4534-8e12-1caad921bda1',
-    name: 'Sharlene Bush',
-    email: 'sharlenebush@tubesys.com',
-    eyeColor: 'blue',
-    friends: ['Briana Decker', 'Sharron Pace'],
-    isActive: true,
-    balance: 3821,
-    skills: ['tempor', 'mollit', 'commodo', 'veniam', 'laborum'],
-    gender: 'female',
-    age: 34,
-  },
-  {
-    id: '88beb2f3-e4c2-49f3-a0a0-ecf957a95af3',
-    name: 'Ross Vazquez',
-    email: 'rossvazquez@xinware.com',
-    eyeColor: 'green',
-    friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
-    isActive: false,
-    balance: 3793,
-    skills: ['nulla', 'anim', 'proident', 'ipsum', 'elit'],
-    gender: 'male',
-    age: 24,
-  }]
 
-const getUsersWithGender = (array, gender) => array.find(function ( elem ) {
-  let ret;
-  if (elem.gender === gender) {
-    ret = elem.name
-  }
-  // return ret.map(function (i) {
-  //   return ret
-  // }) 
-  return ret
-                      // Тут шото не получается
-  
-});
 
-console.log(getUsersWithGender(users, 'male'));
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> Задача с ДЗ - 3 >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+/* 
+*
+filter, map
+
+Получи массив имен пользователей (значение свойства name) по полу (значение свойства gender).
+
+Используй деструктурирующее присваивание для параметра функции ({name}) без пробелов и переносов на новую строку.
+
+Используй только перебирающие методы массива которые не изменяют (не мутируют) исходный массив.
+ Т.е. нельзя использовать for, splice, push и т.п. мутирующие методы. */
+
+// const users = [
+//   {
+//     id: '701b29c3-b35d-4cf1-a5f6-8b12b29a5081',
+//     name: 'Moore Hensley',
+//     email: 'moorehensley@indexia.com',
+//     eyeColor: 'blue',
+//     friends: ['Sharron Pace'],
+//     isActive: false,
+//     balance: 2811,
+//     skills: ['ipsum', 'lorem'],
+//     gender: 'male',
+//     age: 37,
+//   },
+//   {
+//     id: '7a3cbd18-57a1-4534-8e12-1caad921bda1',
+//     name: 'Sharlene Bush',
+//     email: 'sharlenebush@tubesys.com',
+//     eyeColor: 'blue',
+//     friends: ['Briana Decker', 'Sharron Pace'],
+//     isActive: true,
+//     balance: 3821,
+//     skills: ['tempor', 'mollit', 'commodo', 'veniam', 'laborum'],
+//     gender: 'female',
+//     age: 34,
+//   },
+//   {
+//     id: '88beb2f3-e4c2-49f3-a0a0-ecf957a95af3',
+//     name: 'Ross Vazquez',
+//     email: 'rossvazquez@xinware.com',
+//     eyeColor: 'green',
+//     friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
+//     isActive: false,
+//     balance: 3793,
+//     skills: ['nulla', 'anim', 'proident', 'ipsum', 'elit'],
+//     gender: 'male',
+//     age: 24,
+//   },  {
+//     id: '701b29c3-b35d-4cf1-a5f6-8b12b29a5081',
+//     name: 'Mre Hey',
+//     email: 'moorehensley@indexia.com',
+//     eyeColor: 'blue',
+//     friends: ['Sharron Pace'],
+//     isActive: false,
+//     balance: 2811,
+//     skills: ['ipsum', 'lorem'],
+//     gender: 'male',
+//     age: 37,
+//   }]
+
+//  >>>>>>>>>>>>>>>>  - решение обычной функцией
+
+//   const getUsersWithGender = (array, gender) => array.filter(function ( elem ) {
+   
+//     if (elem.gender === gender) {
+//       return elem
+//     }
+//   }).map(function (i) {
+//   return i.name
+// });
+
+// >>>>>>>>>>>>>>>>>> -  Решение стрелочной функцией
+// const getUsersWithGender = (array, gender) => array.filter(array => array.gender === gender).map(({name}) => name)  
+// console.log(getUsersWithGender(users, 'male'));
 
 /* [
   "Moore Hensley",
@@ -3027,4 +3053,121 @@ console.log(getUsersWithGender(users, 'male'));
   "Carey Barr",  
   "Blackburn Dotson"  
 ] */
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> Задача с ДЗ - 4 >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+/* 
+*
+*
+*
+*
+*
+filter и оператор !
+
+Получи массив только неактивных пользователей (отфильтруй по значению свойства isActive)
+
+Используй деструктурирующее присваивание для параметра функции ({isActive}) без пробелов и переносов на новую строку.
+
+Используй оператор !.
+
+Используй только перебирающие методы массива которые не изменяют (не мутируют) исходный массив.
+ Т.е. нельзя использовать for, splice, push и т.п. мутирующие методы.
+// */
+// const users = [
+//   {
+//     id: '701b29c3-b35d-4cf1-a5f6-8b12b29a5081',
+//     name: 'Moore Hensley',
+//     email: 'moorehensley@indexia.com',
+//     eyeColor: 'blue',
+//     friends: ['Sharron Pace'],
+//     isActive: true,
+//     balance: 2811,
+//     skills: ['ipsum', 'lorem'],
+//     gender: 'male',
+//     age: 37,
+//   },
+//   {
+//     id: '88beb2f3-e4c2-49f3-a0a0-ecf957a95af3',
+//     name: 'Ross Vazquez',
+//     email: 'rossvazquez@xinware.com',
+//     eyeColor: 'green',
+//     friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
+//     isActive: false,
+//     balance: 3793,
+//     skills: ['nulla', 'anim', 'proident', 'ipsum', 'elit'],
+//     gender: 'male',
+//     age: 24,
+//   }]
+
+// const getInactiveUsers = array => array.filter(({ isActive }) => !isActive)
+
+
+// // >>>>>>>>>> Подзадача( тоже что и выше только возвращаем имена неактивных пользователей)
+// //const getInactiveUsers = array => array.filter(({ isActive }) => !isActive) .map(array => array.name)
+// console.log(getInactiveUsers(users));
+
+
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> Задача с ДЗ - 5 >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+/* 
+*
+*
+*
+*
+find
+
+Получи объект пользователя (не массив) по уникальному значению свойства email.
+
+Используй деструктурирующее присваивание для параметра функции ({email}) без пробелов и переносов на новую строку.
+
+Используй только перебирающие методы массива которые не изменяют (не мутируют) исходный массив. Т.е. нельзя использовать for, splice, push и т.п. мутирующие методы. */
+
+// const users = [
+//  {
+//     id: '249b6175-5c30-44c6-b154-f120923736f5',
+//     name: 'Ahead',
+//     email: 'ahead@omatom.com',
+//     eyeColor: 'green',
+//     friends: ['Goldie Gentry', 'Aisha Tran'],
+//     isActive: true,
+//     balance: 2278,
+//     skills: ['adipisicing', 'irure', 'velit'],
+//     gender: 'female',
+//     age: 21,
+//   }, {
+//     id: '88beb2f3-e4c2-49f3-a0a0-ecf957a95af3',
+//     name: 'Ross Vazquez',
+//     email: 'rossvazquez@xinware.com',
+//     eyeColor: 'green',
+//     friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
+//     isActive: false,
+//     balance: 3793,
+//     skills: ['nulla', 'anim', 'proident', 'ipsum', 'elit'],
+//     gender: 'male',
+//     age: 24,
+//   },
+//   {
+//     id: '249b6175-5c30-44c6-b154-f120923736f5',
+//     name: 'Elma Head',
+//     email: 'elmahead@omatom.com',
+//     eyeColor: 'green',
+//     friends: ['Goldie Gentry', 'Aisha Tran'],
+//     isActive: true,
+//     balance: 2278,
+//     skills: ['adipisicing', 'irure', 'velit'],
+//     gender: 'female',
+//     age: 21,
+//   }]
+
+// const getUserWithEmail = (array, mail)  => array.find(({email})=> mail === email)  //Решение стрелками
+
+
+// //Решение стрелками + класическая функц
+// // const getUserWithEmail = (array, mail)  => array.find(function (elem) {
+// //  return (mail=== elem.email)
+// // }) 
+
+  
+
+// console.log(getUserWithEmail(users, 'rossvazquez@xinware.com'));
 
